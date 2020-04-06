9c3fa50eb7ebd9d4f6e9f18eb6e01eb117ef06b3
realmarv <zahratehraninasab@gmail.com>
2020-04-06 13:52:20 +0430

    implement search method

---
+++ kvstore.py
@@ -0,0 +1,28 @@
+
+class Node:
+    
+    def __init__(self, key, value, isend):
+        self.key = key
+        self.value = value
+        self.children = []
+        self.isend = isend
+
+    def insert(self, key, value):
+        childnode = children[key[0]]
+
+
+class Trie:
+    root
+
+    def __init__():
+        root = []
+
+    def insert(self, key, value):
+        pass
+
+    def search(self, key):
+
+        for node, i in root, range(0, len(key)):
+            if
+
+
