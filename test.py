6684c43b4cc77aad59985d6e12cb1dfcbc11301f
realmarv <zahratehraninasab@gmail.com>
2020-04-07 02:06:25 +0430

    add testmiddle method

--- test.py
+++ test.py
@@ -17,6 +17,26 @@ class TestInsert(unittest.TestCase):
         value = trie.root.children['z'].children['h'].children['r'].children['a'].value
         self.assertEqual('musavi', value)
 
+    def testmiddle(self):
+        trie = kvstore.Trie()
+        trie.insert('abcd', 'first')
+        trie.insert('abe', 'second')
+        
+        value1 = trie.root.children['a'].children['b'].children['c'].children['d'].value
+        self.assertEqual(value1, 'first')
+        
+        value2 = trie.root.children['a'].children['b'].children['e'].value
+        self.assertEqual(value2, 'second')
+
+
+class Search(unittest.TestCase):
+    
+    def testsearch(self):
+        trie = kvstore.Trie()
+        trie.insert('zahra', 'tehrani')
+        value = trie.search('zahra')
+        self.assertEqual(value, 'tehrani')
+
 
 if __name__ == '__main__':
     unittest.main()
