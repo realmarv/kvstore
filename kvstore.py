6733d268367b549b12b5e670d99817f8a045d59b
realmarv <zahratehraninasab@gmail.com>
2020-04-07 20:51:11 +0430

    implement delete method and raise exceptions in insert and search methods

--- kvstore.py
+++ kvstore.py
@@ -14,10 +14,13 @@ class Trie:
     def insert(self, key, value):
         '''
         Insert a key value to the trie data structure
-        Key must be string.
+        Raises EmptyStringException if key is an empty string.
+        Raises NotStringException if key is not an string.
         '''
         if not isinstance(key, str):
             raise NotStringException()
+        elif not key:
+            raise EmptyStringException()
 
         node = self.root
         for i in range(len(key)):
@@ -44,8 +47,15 @@ class Trie:
     def search(self, key):
         '''
         Search a value in dictionary by it's key
-        Returns None if the key doesn't exist in the dictionary.
+        Returns None if key doesn't exist in dictionary
+        Raises EmptyString exception if key is an empty string.
+        Raises NotStringException if key is not an string.
         '''
+        if not isinstance(key, str):
+            raise NotStringException()
+        elif not key:
+            raise EmptyStringException()
+
         node = self.root
         for i in range(len(key)):
             try:
@@ -57,7 +67,33 @@ class Trie:
             except:
                 return None
 
+    def delete(self, key):
+        '''
+        Delete a key, value pair by the key from the trie
+        Raises a KeyError if the key doesn't exist in the trie.
+        '''
+        if not isinstance(key, str):
+            raise NotStringException()
+        elif not key:
+            raise EmptyStringException()
 
+        removeinfo = (self.root, key[0]) 
+        node = self.root
+        for i in range(len(key)):
+            if len(node.children) > 1 or node.value:
+                removeinfo = (node, key[i])
+           
+            node = node.children[key[i]]
+
+        if node.children:
+            node.value = None
+        else:
+            father = removeinfo[0]
+            char = removeinfo[1]
+
+            del father.children[char]
+                
+                
 class NotStringException(Exception): pass
-    
+class EmptyStringException(Exception): pass
 
